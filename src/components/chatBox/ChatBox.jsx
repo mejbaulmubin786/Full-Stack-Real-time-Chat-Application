750a65c62aec00ffd4a0036ab2bcc48889a17c35
Mejbaul Mubin <mejbaul_mubin@yahoo.com>
2025-10-12 23:27:26 +0600

    chatbox file update

--- src/components/chatBox/ChatBox.jsx
+++ src/components/chatBox/ChatBox.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './ChatBox.css'
+import assets from '../../assets/assets'
 
 const ChatBox = () => {
   return (
-    <div>ChatBox</div>
+    <div className='chat-box'>
+      <div className="chat-user">
+        <img src={assets.profile_img} alt="" />
+        <p>Mejbaul  Mubin<img className='dot' src={assets.green_dot} alt="" /></p>
+        <img src={assets.help_icon} className='help' alt="" />
+      </div>
+      <div className="chat-input">
+        <input type="text" placeholder='Send a message'/>
+        <input type="file" name="" id="image" accept='image/png, image/jpeg' hidden/>
+        <label htmlFor="image">
+          <img src={assets.gallery_icon} alt="" />
+        </label>
+        <img src={assets.send_button} alt="" />
+      </div>
+    </div>
   )
 }
 
