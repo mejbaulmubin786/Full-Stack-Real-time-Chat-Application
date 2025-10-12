20f364cb1c02a5343d026f6fb1e35f9807106960
Mejbaul Mubin <mejbaul_mubin@yahoo.com>
2025-10-12 22:10:01 +0600

    left side-bar update

--- src/components/chatBox/ChatBox.jsx
+++ src/components/chatBox/ChatBox.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './ChatBox.css'
 
 const ChatBox = () => {
   return (
--- src/pages/chat/Chat.jsx
+++ src/pages/chat/Chat.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './Chat.css'
+import LeftSideBar from '../../components/leftSideBar/LeftSideBar'
+import ChatBox from '../../components/chatBox/ChatBox'
+import RightSideBar from '../../components/rightSideBar/RightSideBar'
 const Chat = () => {
   return (
     <div className='chat'>
       <div className="chat-container">
-        
+        <LeftSideBar/>
+        <ChatBox />
+        <RightSideBar />
+
       </div>
     </div>
   )
