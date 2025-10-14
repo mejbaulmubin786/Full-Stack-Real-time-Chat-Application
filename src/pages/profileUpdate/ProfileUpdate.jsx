b589c4474a60bc204b95d9b71327cb422fa38910
Mejbaul Mubin <mejbaul_mubin@yahoo.com>
2025-10-14 12:48:21 +0600

    Update

--- src/pages/profileUpdate/ProfileUpdate.jsx
+++ src/pages/profileUpdate/ProfileUpdate.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './ProfileUpdate.css'
+import assets from '../../assets/assets'
 
 const ProfileUpdate = () => {
   return (
-    <div>ProfileUpdate</div>
+    <div className='profile'>
+      <div className="profile-container">
+        <form>
+          <h3>Profile Details</h3>
+          <label htmlFor="avatar">
+            <input type="file" id='avatar' accept='.png, .jpg, jpeg' higgen/>
+            <img src={assets.avatar_icon} alt="" />
+          </label>
+        </form>
+      </div>
+    </div>
   )
 }
 
